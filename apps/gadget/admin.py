71072f374f19176b8d3bcba7a62f451985764737
Vadim Chernysh <chernysh.vadim@gmail.com>
2011-08-19 12:42:47 +0300

    Initial commit

--- apps/gadget/admin.py
+++
@@ -1,4 +0,0 @@
-from gadget.models import Playlist
-from django.contrib import admin
-
-admin.site.register(Playlist)
--- apps/kcms/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('kcms',
-	(r'^$', 'views.index'),
-	(r'(?P<page>\w+)/$', 'views.subpage'),
-)
